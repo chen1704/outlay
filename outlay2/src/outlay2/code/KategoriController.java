c1db1af28eb53e71e11dd858bd631d6cdffcd20c
chen1704 <32873349+chen1704@users.noreply.github.com>
2018-12-04 15:39:24 +0700

    Add files via upload

---
+++ outlay2/src/outlay2/code/KategoriController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outlay2.code;
+
+/**
+ *
+ * @author core i5
+ */
+public class KategoriController {
+   private Kategori kategori;
+   
+   
+   
+}
---
+++ outlay2/src/outlay2/db/connector.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outlay2.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+/**
+ *
+ * @author core i5
+ */
+public class connector {
+    public static Connection getConnection() throws Exception{
+        Connection cn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        String query = "";
+        try{
+            cn = DriverManager.getConnection("jdbc:derby://localhost:1527/outlaydb", "test", "test");
+            st = cn.createStatement();
+            rs = st.executeQuery(query);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return cn;
+    }
+}
